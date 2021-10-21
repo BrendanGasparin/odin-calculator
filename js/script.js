e89564ca22e448e26a9510893cac123bcc4ff85d
Brendan Gasparin <brendan.gasparin@gmail.com>
2021-10-22 05:33:28 +1100

    Implement math functions but not equals sign

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 let currentNum = document.querySelector('input[type="text"]').value || 0;
+let lastNum = 0;
+let lastOperator = '';
+let currentOperator = '';
+let resetNum = false;
 
 function add(a, b) {
     return a + b;
@@ -21,17 +25,25 @@ function operate(operator, num1, num2) {
         return add(num1, num2);
     } else if (operator === '-') {
         return subtract(num1, num2);
-    } else if (operator === '*') {
+    } else if (operator === 'x') {
         return multiply(num1, num2);
     } else if (operator === '/') {
         return divide(num1, num2);
     }
+
+    lastOperator = '';
 }
 
 function handleButtons(e) {
+    console.log(e.target.value);
     const input = document.querySelector('input[type="text"]');
     if (!isNaN(e.target.value)) {
-        input.value = `${input.value}${e.target.value}`;
+        if(!resetNum) {
+            input.value = `${input.value}${e.target.value}`;
+        } else {
+            input.value = e.target.value;
+            resetNum = false;
+        }
         cleanseInput();
     }
     if (e.target.value === '.') {
@@ -40,6 +52,20 @@ function handleButtons(e) {
     }
     if (e.target.value === 'AC') {
         input.value = '0';
+        cleanseInput();
+    }
+    if (e.target.value === '+' || e.target.value === '-' || e.target.value === 'x' || e.target.value === '/') {
+        if (lastOperator === '') {
+            lastOperator = e.target.value;
+            lastNum = input.value;
+            resetNum = true;
+        } else {
+            currentOperator = e.target.value;
+            currentNum = operate(lastOperator, lastNum, currentNum);
+            lastNum = 0;
+            lastOperator = '';
+            input.value = currentNum;
+        }
     }
     currentNum = input.value;
 }
